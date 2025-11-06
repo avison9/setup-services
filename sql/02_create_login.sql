1c07b9592b1df01b4c29a62ea57870218647faa0
avison <avisonlivingstone@gmail.com>
2025-11-06 15:22:45 +0000

    working sql script

--- sql/02_create_login.sql
+++ sql/02_create_login.sql
@@ -1,3 +1,7 @@
+-- 02_create_login.sql
+-- Creates LOGIN role in the *postgres* DB
+-- Called with: -v tenant=oraion -v suffix=analytics -v password="..."
+
 DO $$
 BEGIN
    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'tenant' || '-' || :'suffix') THEN
