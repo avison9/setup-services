6aaa1b4c8afbaf497cfa1ee7ea1212e811f7afc8
avison <avisonlivingstone@gmail.com>
2025-11-06 19:30:13 +0000

    parsing

--- sql/03_grant_permissions.sql
+++ sql/03_grant_permissions.sql
@@ -5,18 +5,16 @@
 \echo Applying grants for tenant :'tenant' / suffix :'suffix'
 
 -- ------------------------------------------------------------------
--- Set **boolean** flags (true / false) – this is the ONLY way \if works
+-- Set boolean flags (true / false) – only way \if works
 -- ------------------------------------------------------------------
--- Set default
 \set is_analytics false
 \set is_ai false
 \set is_application false
 
--- Use SQL to determine which one is true
+-- Determine which one is true
 SELECT 
     CASE :'suffix'
         WHEN 'analytics' THEN true
-        WHEN 'ai' THEN false
         ELSE false
     END as is_analytics,
     CASE :'suffix'
@@ -35,28 +33,23 @@ SELECT
 \if :is_analytics
     \connect :"tenant"_analytics
 
-    -- build role name dynamically
+    -- build role name
     \set role_name :"tenant"_analytics
-    \set dbname :"tenant"_analytics
 
     -- create role if missing
     SELECT format('CREATE ROLE %I', :'role_name')
     WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'role_name')
     \gexec
 
-    DO
-    $$
-    BEGIN
-       EXECUTE format('GRANT ALL ON DATABASE %I TO %I;', current_database(), :'role_name');
-    END
-    $$;
+    -- grant all privileges on current database
+    SELECT format('GRANT ALL ON DATABASE %I TO %I', current_database(), :'role_name') \gexec
 
     -- grant default privileges on tables
     SELECT format(
         'ALTER DEFAULT PRIVILEGES IN SCHEMA public, raw, dwh, data_access_layer ' ||
-        'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I;',
+        'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I',
         :'role_name'
-    ) \gexec;
+    ) \gexec
 \endif
 
 -- ------------------------------------------------------------------
@@ -65,26 +58,19 @@ SELECT
 \if :is_ai
     \connect :"tenant"_ai
 
-    -- build role name dynamically
     \set role_name :"tenant"_ai
-    \set dbname :"tenant"_ai
 
     SELECT format('CREATE ROLE %I', :'role_name')
     WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'role_name')
     \gexec
 
-    DO
-    $$
-    BEGIN
-       EXECUTE format('GRANT ALL ON DATABASE %I TO %I;', current_database(), :'role_name');
-    END
-    $$;
+    SELECT format('GRANT ALL ON DATABASE %I TO %I', current_database(), :'role_name') \gexec
 
     SELECT format(
         'ALTER DEFAULT PRIVILEGES IN SCHEMA public ' ||
-        'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I;',
+        'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I',
         :'role_name'
-    ) \gexec;
+    ) \gexec
 \endif
 
 -- ------------------------------------------------------------------
@@ -93,26 +79,19 @@ SELECT
 \if :is_application
     \connect :"tenant"_application
 
-    -- build role name dynamically
     \set role_name :"tenant"_application
-    \set dbname :"tenant"_application
 
     SELECT format('CREATE ROLE %I', :'role_name')
     WHERE NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'role_name')
     \gexec
 
-    DO
-    $$
-    BEGIN
-       EXECUTE format('GRANT ALL ON DATABASE %I TO %I;', current_database(), :'role_name');
-    END
-    $$;
+    SELECT format('GRANT ALL ON DATABASE %I TO %I', current_database(), :'role_name') \gexec
 
     SELECT format(
         'ALTER DEFAULT PRIVILEGES IN SCHEMA public ' ||
-        'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I;',
+        'GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO %I',
         :'role_name'
-    ) \gexec;
+    ) \gexec
 \endif
 
 \echo Done granting permissions for :'tenant'_:'suffix'
