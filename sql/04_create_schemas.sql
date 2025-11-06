8b0f3ec64ed84c13c219d5e7a6b43b778cb60e48
avison <avisonlivingstone@gmail.com>
2025-11-06 17:19:31 +0000

    new working script

--- sql/04_create_schemas.sql
+++ sql/04_create_schemas.sql
@@ -1,14 +1,18 @@
--- 04_create_schemas.sql
--- Creates schemas & sets search_path, safely
-
-\echo Ensuring schemas and search path for :'tenant'-:'suffix'
+\if :suffix = analytics
+\connect :"tenant"-analytics
 
 CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS dwh;
 CREATE SCHEMA IF NOT EXISTS data_access_layer;
 
--- Use format() to combine identifiers safely
+-- Set search_path for administrator and tenant-analytics only
+SELECT format(
+  'ALTER ROLE %I SET search_path TO raw, dwh, data_access_layer, public',
+  'administrator'
+) \gexec;
+
 SELECT format(
   'ALTER ROLE %I SET search_path TO raw, dwh, data_access_layer, public',
   :'tenant' || '-' || :'suffix'
-)\gexec
+) \gexec;
+\endif
