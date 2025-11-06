954d1bccdcc8735d708ddf983e9c68c3730569e2
avison <avisonlivingstone@gmail.com>
2025-11-06 17:41:49 +0000

    new working script

--- sql/04_create_schemas.sql
+++ sql/04_create_schemas.sql
@@ -1,25 +1,20 @@
--- Set boolean flags based on suffix
-\set is_analytics false
+-- ------------------------------------------------------------------
+-- Schema creation – **only** for analytics
+-- ------------------------------------------------------------------
+\set is_analytics  false
 
 \if :suffix = analytics
     \set is_analytics true
 \endif
 
--- Schema creation only for analytics
 \if :is_analytics
-    \connect :"tenant"-analytics
+    \connect :"tenant"_analytics
+
     CREATE SCHEMA IF NOT EXISTS raw;
     CREATE SCHEMA IF NOT EXISTS dwh;
     CREATE SCHEMA IF NOT EXISTS data_access_layer;
 
-    -- Set search_path for administrator and tenant-analytics
-    SELECT format(
-      'ALTER ROLE %I SET search_path TO raw, dwh, data_access_layer, public',
-      'administrator'
-    ) \gexec;
-
-    SELECT format(
-      'ALTER ROLE %I SET search_path TO raw, dwh, data_access_layer, public',
-      :'tenant' || '_' || :'suffix'
-    ) \gexec;
+    -- search_path for admin and the analytics role
+    SELECT format('ALTER ROLE %I SET search_path TO raw, dwh, data_access_layer, public', 'administrator') \gexec;
+    SELECT format('ALTER ROLE %I SET search_path TO raw, dwh, data_access_layer, public', :'tenant' || '_analytics') \gexec;
 \endif
