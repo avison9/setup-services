4bf929f63e8d295f775d58a8e650015e78df06a0
avison <avisonlivingstone@gmail.com>
2025-11-06 16:26:56 +0000

    working sql create db

--- sql/04_create_schemas.sql
+++ sql/04_create_schemas.sql
@@ -1,9 +1,11 @@
 -- 04_create_schemas.sql
--- Must be run *inside* the target database
+-- Creates base schemas and sets role search_path
 -- Called with: -v tenant=oraion -v suffix=analytics
+
+\echo Creating schemas and configuring role for :'tenant'-:'suffix'
 
 CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS dwh;
 CREATE SCHEMA IF NOT EXISTS data_access_layer;
 
-ALTER ROLE :"tenant-suffix" SET search_path TO raw, dwh, data_access_layer, public;
+ALTER ROLE :"tenant"-:"suffix" SET search_path TO raw, dwh, data_access_layer, public;
